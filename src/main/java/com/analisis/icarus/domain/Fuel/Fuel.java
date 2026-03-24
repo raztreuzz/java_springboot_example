61b4116e14677ba6619a9d557563f8176b15110f
raztreuzz <raztreuzz1234@gmail.com>
2026-03-24 00:45:13 -0600

    fix(fuel): restore rules of consumptionRate

--- src/main/java/com/analisis/icarus/domain/Fuel/Fuel.java
+++ src/main/java/com/analisis/icarus/domain/Fuel/Fuel.java
@@ -1,15 +1,19 @@
 package com.analisis.icarus.domain.Fuel;
 
 public interface Fuel {
-
     String getName();
 
     float getCapacity();
-
     float getEfficiency();
 
-    float getTotalDurationMinutes();
+    default float calculateDuration(float consumptionRate){
+        if (consumptionRate <= 0){
+           throw new IllegalArgumentException("La cantidad de consumo debe ser mayor a cero");
+        }
+        float effectiveCapacity = getCapacity() * getEfficiency();
+        return effectiveCapacity / consumptionRate;
+    }
 
-    PowerType getType();
+    float getTotalDurationMinutes();
 
 }
